38c4941638aef9351831e2d1cf8ad5d3c25fd08b
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-07 12:38:09 -0300

    src:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom'
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
     <div>
 
-        
+        <ComFilhos>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </ComFilhos>
         {/* <Primeiro/>
         <ComParametro titulo="titulo" subtitulo="subtitulo"/>
         <ComParametro titulo="titulo" subtitulo="subtitulo"/>
