b4269c8d852c76db57adb372b22bf6a995351310
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-07 12:23:35 -0300

    .

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function() {
+    return (
+        <div>
+            <h1>Primeiro</h1>
+            <h2>Exemplo de componente</h2>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+
+ReactDOM.render(
+    <div>
+        <Primeiro/>
+        <ComParametro titulo="titulo" subtitulo="subtitulo"/>
+        <ComParametro titulo="titulo" subtitulo="subtitulo"/>
+        <ComParametro titulo="titulo" subtitulo="subtitulo"/>
+        <ComParametro titulo="titulo" subtitulo="subtitulo"/>
+        <ComParametro titulo="titulo" subtitulo="subtitulo"/>
+        
+    </div>,
+    document.getElementById('root')
+)
